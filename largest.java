66006d4545bb078d76cf8c58fb144dffddc351f7
pavithrapalanisamy <p.pavithra12349@gmail.com>
2017-06-14 12:59:11 +0530

    Add files via upload

---
+++ largest.java
@@ -0,0 +1,29 @@
+package hello;
+
+import java.util.Scanner;
+
+public class largest {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		System.out.println("enter the number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+		if((a>=b)&&(a>=c)){
+			System.out.println("a is large number");
+		}
+		else if((b>=a)&&(b>=c))
+		{
+			System.out.println("b is large number");
+			
+		}
+		else
+		{
+			System.out.println("c is large number");
+		}
+
+	}
+
+}
